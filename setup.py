21af487508909686780f36551e6ef9fd6d1f1dc3
kukree <kulevgen32@gmail.com>
2020-03-11 02:37:17 +0600

    Add license into setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setup(
     author_email='kulevgen32@gmail.com',
     version=turba.__version__,
     packages=['turba'],
+    license='MIT',
     classifiers=[
         'Environment :: Console',
-        'Topic :: System :: Emulators'
+        'Topic :: System :: Emulators',
+        'License :: OSI Approved :: MIT License'
     ]
 )
